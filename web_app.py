c1b99c0fcce28c0209a5ca11c4a9a386d44c21e2
aayanamir66-creator <aayanamir66@gmail.com>
2026-09-06 18:05:39 +0500

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -6,7 +6,7 @@ from groq import Groq
 st.set_page_config(page_title="A.ai", page_icon="🤖")
 
 st.title("🤖 A.ai Intelligence System")
-st.write("Developed by Aayan • Advanced ChatGPT-Style Engine Active.")
+st.write("Developed by Aayan • best AI.")
 
 # 2. Strict Isolated Groq Client Initialization Layout
 # Forces Streamlit to look ONLY at your explicit Secrets Dashboard key
